4327a6a898626fdafd9de5299957551ade51170c
Wesley Maia <maia.weol@gmail.com>
2022-06-26 07:17:30 -0700

    Create TopicoRepository

--- src/main/java/br/com/project/forum/controller/TopicosController.java
+++ src/main/java/br/com/project/forum/controller/TopicosController.java
@@ -1,22 +1,34 @@
 package br.com.project.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.project.forum.dto.TopicoDTO;
-import br.com.project.forum.modelo.Curso;
 import br.com.project.forum.modelo.Topico;
+import br.com.project.forum.repository.TopicoRepository;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicosController {
 
+  @Autowired
+  private TopicoRepository topicoRepository;
+
   @GetMapping
-  public List<TopicoDTO> lista() {
-     Topico topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
-     return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+  public List<TopicoDTO> lista(String nomeCurso) {
+    
+    if (nomeCurso == null) {
+      System.out.println("null");
+      List<Topico> topicos = topicoRepository.findAll();
+      return TopicoDTO.converter(topicos);
+    } else {
+      System.out.println(nomeCurso);
+      List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso); 
+      return TopicoDTO.converter(topicos);
+    }
+    
   }
-  
+
 }
